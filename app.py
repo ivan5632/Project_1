b8804bcc0a0f2f0e3e6d061b9fd6b9f872e26c93
Ivan Budnikov <ivan34543799@gmail.com>
2025-10-09 17:44:03 +0200

    created with gemini

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    df = pd.read_excel('data.xlsx')
+    return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+
+if __name__ == '__main__':
+    app.run(debug=True)
