b09be7d71afbf8bf9120ca0e680165acbf0e76e5
Ivan Budnikov <ivan34543799@gmail.com>
2025-10-15 17:46:48 +0200

    updated requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
-import pandas as pd
 
+# Initialize the Flask application
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    df = pd.read_excel('data.xlsx')
-    return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+    """
+    The main route that renders the HTML template.
+    This template contains the button and the JavaScript logic for the message.
+    """
+    # Flask automatically looks for 'index.html' inside the 'templates' folder.
+    return render_template('index.html')
 
+# Run the application if the script is executed directly
 if __name__ == '__main__':
+    # Setting debug=True allows the server to reload automatically on code changes
+    # and provides helpful error messages.
     app.run(debug=True)
