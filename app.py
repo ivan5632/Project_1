d281489d82548fe2b806db4deb17c69c88651f5f
Ivan Budnikov <ivan34543799@gmail.com>
2025-10-16 20:21:44 +0200

    mockup app flask-gunicorn-nginx-docker - simple button and a message

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 from flask import Flask, render_template
 
-# Initialize the Flask application
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """
-    The main route that renders the HTML template.
-    This template contains the button and the JavaScript logic for the message.
-    """
-    # Flask automatically looks for 'index.html' inside the 'templates' folder.
+    # Renders the index.html file from the templates directory
     return render_template('index.html')
 
-# Run the application if the script is executed directly
 if __name__ == '__main__':
-    # Setting debug=True allows the server to reload automatically on code changes
-    # and provides helpful error messages.
-    app.run(debug=True)
+    # We will use Gunicorn for production, but this is fine for local testing
+    app.run(host='0.0.0.0', port=5000)
+
+
